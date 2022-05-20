8aa043a5f1a5d77b994cdaa6134745ea3fd72890
hhchaos <HHChaos@outlook.com>
2022-05-20 14:57:35 +0800

    添加项目文件。

---
+++ WinUITestDemo/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WinUITestDemo
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Root.Children.Remove(TestProgressRing);
+            await Task.Delay(200);
+            Root.Children.Add(TestProgressRing);
+        }
+    }
+}
